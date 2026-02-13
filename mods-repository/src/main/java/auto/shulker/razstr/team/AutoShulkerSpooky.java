d675b81f34686fcfb509e639b720a5f715077e18
RaZrad <164731463+RaZrad@users.noreply.github.com>
2026-02-13 19:33:29 +0000

    Revert accidental KeyMapping change

--- mods-repository/src/main/java/auto/shulker/razstr/team/AutoShulkerSpooky.java
+++ mods-repository/src/main/java/auto/shulker/razstr/team/AutoShulkerSpooky.java
@@ -6,13 +6,13 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 import org.lwjgl.glfw.GLFW;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyMapping;
+import net.minecraft.client.option.KeyBinding;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import auto.shulker.razstr.team.ElytraEquipLogic;
 
 public class AutoShulkerSpooky implements ModInitializer, ClientModInitializer {
 
-    public static KeyMapping autoCatKey;
+    public static KeyBinding autoCatKey;
 
     @Override
     public void onInitialize() {
@@ -21,7 +21,7 @@ public class AutoShulkerSpooky implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        autoCatKey = new KeyMapping(
+        autoCatKey = new KeyBinding(
                 "key.autoshulker.auto_cat",
                 GLFW.GLFW_KEY_0,
                 "category.autoshulker"
